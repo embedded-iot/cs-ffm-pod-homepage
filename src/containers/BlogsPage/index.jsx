94f1c81e88f057f6ecbe1029d49ce2bf282ab0e1
nguyenquan5895 <nguyenquan5895@gmail.com>
2023-12-22 23:12:44 +0700

    Add blogs page to ssr

--- src/containers/BlogsPage/index.jsx
+++ src/containers/BlogsPage/index.jsx
@@ -2,20 +2,19 @@
 
 import React from 'react';
 import BlogsGrid from 'components/FrontUser/BlogsGrid';
-import { useMediaQuery } from 'react-responsive';
-import { RESPONSIVE_MEDIAS } from 'components/contants';
 import {useParams, useRouter} from "next/navigation";
 import "./style.scss";
 
 
-function BlogsPage(props) {
-  const isMobile = useMediaQuery(RESPONSIVE_MEDIAS.MOBILE);
+function BlogsPage({ blogsResponse, blogsCategoriesResponse }) {
   const { blogCategoryId } = useParams();
   const router = useRouter();
   return (
-    <div className={`${isMobile ? 'page-wrapper--full-width' : 'page-wrapper'} blogs-page__wrapper`}>
+    <div className={`page-wrapper blogs-page__wrapper`}>
       <div className="page-contents">
         <BlogsGrid
+          blogsResponse={blogsResponse}
+          blogsCategoriesResponse={blogsCategoriesResponse}
           blogCategoryId={blogCategoryId ? +blogCategoryId : ''}
           redirectTo={router.push}
         />
